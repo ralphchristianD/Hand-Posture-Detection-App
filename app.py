7387477299ac975ada3595782f92cf1804be6cb4
Ralph Christian <157346785+ralphchristianD@users.noreply.github.com>
2025-05-18 23:51:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,15 @@
 import streamlit as st
-# import torch
-# from torchvision import transforms
 from PIL import Image
 from ultralytics import YOLO, RTDETR
-#import matplotlib.pyplot as plt
-
-# RetinaNet imports
-# from torchvision.models.detection.retinanet import retinanet_resnet50_fpn, RetinaNetClassificationHead
 
 # Label and color mapping
 label_map = {0: "proper", 1: "improper"}
-# color_map = {0: "green", 1: "red"}
-#label_map_retina = {1: "proper", 2: "improper"}
-#color_map_retina = {1: "green", 2: "red"}
+
 # Load YOLO and RT-DETR models
 yolo_model = YOLO("yolo11l_posture.pt")
 rtdetr_model = RTDETR("rtdetrl_posture.pt")
 rtdetr_model_fingers = RTDETR("rtdetr_fingers.pt")
 
-# Load RetinaNet model
-# @st.cache_resource
-# def load_retinanet_model():
-#   model = retinanet_resnet50_fpn(pretrained=False)
-#    in_features = model.head.classification_head.cls_logits.in_channels
-#    num_anchors = model.head.classification_head.num_anchors
-#    model.head.classification_head = RetinaNetClassificationHead(
-#       in_channels=in_features,
-#        num_anchors=num_anchors,
-#        num_classes=3  # Background, proper, improper
-#    )
-#    model = torch.load(
-#        "retinanet_full_model_posture.pth",
-#        map_location="cpu"
-#    )
-#    model.eval()
-#    return model
-
-#retina_model = load_retinanet_model()
-
-
-# Preprocessing
-# transform = transforms.Compose([transforms.ToTensor()])
 
 # App UI
 st.title('Hand Posture Detection Using Deep Learning from Image')
@@ -79,48 +48,6 @@ if uploaded_file:
                 cls_id = int(box.cls[0].item())
                 if label_map.get(cls_id, "") == "improper":
                     warning = True
-        '''    
-        elif model_choice == "RetinaNet":
-            retina_model.eval()
-            img_tensor = transform(image).unsqueeze(0)
-            with torch.no_grad():
-                outputs = retina_model(img_tensor)[0]
-
-            boxes = outputs['boxes'].cpu().numpy()
-            scores = outputs['scores'].cpu().numpy()
-            labels = outputs['labels'].cpu().numpy()
-
-            # Apply threshold manually 
-            keep = (scores >= threshold_re) & (labels != 0) # 
-            valid_boxes = boxes[keep]
-            valid_scores = scores[keep]
-            valid_labels = labels[keep]
-
-            fig, ax = plt.subplots(figsize=(10, 10))
-            ax.imshow(image)
-
-            for i in range(len(valid_boxes)):
-                x1, y1, x2, y2 = valid_boxes[i]
-                label_id = valid_labels[i]
-                score = valid_scores[i]
-                label_name = label_map_retina.get(label_id, str(label_id))
-                color = color_map_retina.get(label_id, "green")
-
-                ax.add_patch(plt.Rectangle(
-                    (x1, y1), x2 - x1, y2 - y1,
-                    linewidth=2, edgecolor=color, facecolor='none'
-                ))
-                ax.text(
-                    x1, y1 - 5,
-                    f"{label_name}: {score:.2f}",
-                    color=color,
-                    fontsize=12,
-                    bbox=dict(facecolor='white', edgecolor=color, boxstyle='round,pad=0.2')
-                )
-
-            plt.axis('off')
-            st.pyplot(fig)
-        '''
         if warning:
             st.warning("⚠️ Fix your hand position!")
 
